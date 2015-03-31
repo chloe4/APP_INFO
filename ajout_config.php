785b8e26f0190e3af287630cf226ab22953f6b7c
caroline <caroline.nopper@laposte.net>
2015-03-31 16:33:56 +0200

    enlever les champs non obligatoires

--- ajout_config.php
+++ ajout_config.php
@@ -5,14 +5,12 @@ session_start();
 //Vérification si article, variété, nombre d'articles, prix et durée de vie existe !
 	if (isset($_POST["article_a"])&&
 		isset($_POST["varietes_a"])&&
-		isset($_POST["nb_article_a"])&&
-		isset($_POST["prix_a"])&&
-		isset($_POST["duree_vie_a"])&&
+	
 		
 		) {
 
 $db = new PDO("mysql:host=127.0.0.1;dbname=jsmp","root","");
-$request = $db->prepare("INSERT INTO `utilisateur`(`article_a`, `varietes_a`, `nb_article_a`,`prix_a`, `duree_vie_a`) VALUES (:article_a,:varietes_a,:nb_article_a,:prix_a,:duree_vie_a);");
+$request = $db->prepare("INSERT INTO `Catalogue_Articles`(`article_a`, `varietes_a`, `nb_article_a`,`prix_a`, `duree_vie_a`) VALUES (:article_a,:varietes_a,:nb_article_a,:prix_a,:duree_vie_a);");
 $request->execute(array(
 	"article_a" => $_POST["article_a"],
 	"varietes_a" => $_POST["varietes_a"],
