e098f8f7539113c723264a7ca42f0d1355d757bb
Chloé <chloe.van.cauwenberghe@gmail.com>
2015-04-01 10:31:32 +0200

    Inscription réussie!

--- config.php
+++ config.php
@@ -6,26 +6,47 @@ session_start();
 		isset($_POST["prenom_u"])&&
 		isset($_POST["date_naissance_u"])&&
 		isset($_POST["telephone_u"])&&
+		isset($_POST["email_u"])&&
 		isset($_POST["identifiant_u"])&&
 		isset($_POST["mot_passe_u"])&&
-		isset($_POST["motdepasse2"])&& $_POST["mot_passe_u"] == $_POST["motdepasse2"]&&
-		isset($_POST["email_u"])
-		) {
+		isset($_POST["motdepasse2"])&& $_POST["mot_passe_u"] == $_POST["motdepasse2"]
+		)
+	{
 
-$db = new PDO("mysql:host=127.0.0.1;dbname=jsmp","root","");
-$request = $db->prepare("INSERT INTO 'utilisateur'('nom_u', 'prenom_u', 'date_naissance_u','telephone_u', 'identifiant_u','mot_passe_u', 'email_u') VALUES (:nom_u,:prenom_u,:date_naissance_u,:telephone_u,:identifiant_u,:mot_passe_u,:email_u);");
-$request->execute(array(
-	"nom_u" => $_POST["nom_u"],
-	"prenom_u" => $_POST["prenom_u"],
-	"date_naissance_u" => $_POST["date_naissance_u"],
-	"telephone_u" => $_POST["telephone_u"],
-	"identifiant_u" => $_POST["identifiant_u"],
-	"mot_passe_u" => sha1($_POST["mot_passe_u"]),
-	"email_u" => $_POST["email_u"]
-));
+		// On se connecte à la base de données
+		$db = new PDO("mysql:host=127.0.0.1;dbname=jsmp","root","");
+		
+		// echo "a"; (pour faire des tests)
+		
+		$request = $db->prepare("INSERT INTO `jsmp`.`utilisateur` (`nom_u`,`prenom_u`,`date_naissance_u`,`telephone_u`,`email_u`,`identifiant_u`,`mot_passe_u`) VALUES (:nom_u,:prenom_u,:date_naissance_u,:telephone_u,:email_u,:identifiant_u,:mot_passe_u);");
+		
+		// print_r($_POST); (pour faire des tests, afficher le nom, prénom,..., mot de passe)
+		
+		// permet de prendre la date sous un certain format : ici le format est DD/MM/AAAA
+		$datedenaissancce = DateTime::createFromFormat('d/m/Y',$_POST['date_naissance_u']);
 
-}
-else {
-	echo "<p>Un champ a été oublié</p>";
-    }
+		// Si erreur, affiche l'erreur - permet "d'attraper" l'erreur
+		try {
+			echo "b";
+			$request->execute(array(
+				"nom_u" => $_POST["nom_u"],
+				"prenom_u" => $_POST["prenom_u"],
+				//Permet de transformer le format de la date pour que la base de données puisse le comprendre
+				"date_naissance_u" => $datedenaissancce->format("Y-m-d"),
+				"telephone_u" => $_POST["telephone_u"],
+				"email_u" => $_POST["email_u"],
+				"identifiant_u" => $_POST["identifiant_u"],
+				"mot_passe_u" => sha1($_POST["mot_passe_u"])
+			));
+		
+			// echo "c"; (pour faire des tests)
+		
+		} 
+		catch(PDOException $e) {
+			echo $e->getMessage();
+		}
+
+	}
+
+
 ?>
--- inscription.php
+++ inscription.php
@@ -55,16 +55,16 @@
             ?>
 
             <!--On définit tous les champs nécessaires à l'inscription-->
-            <form action="inscription.php" method="post">
+            <form action="page_principale_test.php" method="post">
            
                 Nom : <input type="text" name="nom_u" /></br>
                 Prénom : <input type="text" name="prenom_u" /></br>
-                Année de naissance (Année/Mois/Jour): <input type="date" name="date_naissance_u" /></br>
+                Année de naissance (JJ/MM/AAAA): <input type="date" name="date_naissance_u" /></br>
                 Téléphone : <input type="int" name="telephone_u" /></br>
+                Email : <input type="email" name="email_u" /></br>
                 Pseudo : <input type="text" name="identifiant_u"/></br>
                 Mot de passe : <input type="password" name="mot_passe_u" /></br>
                 Mot de passe confirmation: <input type="password" name="motdepasse2" /></br>
-                Email : <input type="email" name="email_u" /></br>
 
                 <!--On définit un bouton permettant d'envoyer les informations-->
                 <input type="submit" name="envoyer" Value="S'inscrire"/></br>
