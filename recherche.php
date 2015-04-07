8c53e7a56daf9e637bf61309a3b49bd84e51dce2
alice_samaran@orange.fr <alice_samaran@orange.fr>
2015-04-07 16:47:20 +0200

    Recherche & connection BD & un mouton

--- recherche.php
+++ recherche.php
@@ -1,3 +1,53 @@
+<?php
+//Initialisation de la variable contenant les résultats
+$resultats="";
+$nbreParametres=2;// nbr de para à renseigner 
+if(isset($_POST['query'])&& !empty($_POST['query'])){
+	//si utili rentre qqchose on traite sa requete
+	//on rend clean la requète de l'utilisateur 
+
+$query = preg_replace("#[^a-z ?0-9]#i","",$_POST['query']);
+
+if($_POST['filtre']=="Site entier"){
+	$sql="(SELECT id,blog_title AS title FROM blog WHERE blog_title LIKE ? OR blog_content LIKE ?)UNION(SELECT id,page_title AS title FROM pages WHERE page_title LIKE ? OR page_content LIKE ?)";
+
+
+
+} else if($_POST['filtre']=="Blog"){
+	 $sql="SELECT id,blog_title AS title FROM blog WHERE blog_title LIKE ? OR blog_content LIKE ?";
+
+
+}else if ($_POST['filtre']=="Pages"){
+	$sql="SELECT id,page_title AS title FROM pages WHERE page_title LIKE ? OR page_content LIKE ?";
+
+}
+// connexion à la base de donnée 
+include('connect_db.php');
+
+$req = $db->prepare($sql);
+
+if($nbreParametres==2){
+	$req->execute(array('%'.$query.'%','%'.$query.'%','%'.$query.'%','%'.$query.'%'));
+
+}else {
+	$req->execute(array('%'.$query.'%','%'.$query.'%','%'.$query.'%','%'.$query.'%'));
+
+}
+$count = $req->rowCount();
+if($count >=1){
+	echo"$count résultat(s) trouvé(s) pour<strong>$query</strong><hr/>";
+    while ($data=$req->fetch(PDO::FETCH_OBJ)){
+    	echo '#'.$data->id.'- Titre:'.$data->title.'<br/>';
+
+    }
+}else {
+	echo"0 résultat trouvé pour<strong>$query</strong><hr/>";
+}
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,9 +56,18 @@
 </head>
 <body>
 <form action="<?php echo $_SERVER ['PHP_SELF'];?>" method="post">
-<input type="search" name="query"/>
-</form>
+<label for="query">Entrer votre recherche</label>
+<input type="search" name="query" maxlength="80" size ="80" id="query"/></br>
+Rechercher au niveau de :
+<select name="filtre">
 
+	<option value="Site entier">Site entier </option>
+	<option value="Blog">Blog </option>
+	<option value="Pages">Pages </option>
+</select></br>
+<input type ="submit" value="Rechercher">
+</form>
+<?php echo $resultats;?>
 </body>
 
 
