62562ae0a9dcc1b7f593b3b8870ed11eda9774ef
alice_samaran@orange.fr <alice_samaran@orange.fr>
2015-04-14 14:14:37 +0200

    Bandeau , admin , formulaire : les bons !

--- formulaire_connexion.php
+++ formulaire_connexion.php
@@ -4,7 +4,7 @@
 
         <head>
             <!--On associe à la page html la page CSS-->
-            <link type="text/css" rel="stylesheet" href="page_css.css"/> 
+            <link type="text/css" rel="stylesheet" href="Bandeaucss.css"/> 
             <title> S'inscrire </title>
             <meta charset="utf-8" />
         </head>
@@ -12,16 +12,44 @@
 
         <body>
         <!--On met un fond à notre page-->
-        <body background="http://www.castorama.fr/images/products/h/h_645105.jpg"/> 
+        <body background="http://images99.i.m.pic.centerblog.net/v8v99xxn.jpg"/> 
 
             <!--On définit les boutons d'en-tête de la page-->
-            <div id="header"> 
-                <div id="button"> 
-                    <p style="color: #000000; font-size :16px">Se connecter </p> </div >
-                <div id="button"> 
-                    <p style="color: #FF140C; font-size :16px"><a href="page_principale.html">Accueil</a></p> </div >
-                <div id="button"> 
-                    <p style="color: #FF140C; font-size :16px">Mon panier </p> </div >
+           <ul id="menu">
+    <li><a href="http://localhost/APP_INFO/Bandeau.html">Accueil</a>
+        
+    </li>
+    <li><a href="http://www.mangerbouger.fr/IMG/pdf/inpes_manger_bouger_pdf_produits_saison.pdf">Calendrier</a>
+        
+    </li>
+  <li><a href="#">Acheter/Echanger</a>
+        <ul>
+            
+            <li><a href="http://localhost/APP_INFO/fairemescourses.html#">Nos producteurs</a></li>
+            <li><a href="http://localhost/APP_INFO/Nos_produits.html#">Nos produits</a></li>        
+        </ul>
+        
+    </li><li><a href="#">Des infos !</a>
+        <ul>
+            
+            <li><a href="http://localhost/APP_INFO/Qui_somme_nous.html#">Qui sommes-nous</a></li>
+            <li><a href="http://localhost/APP_INFO/Contact.html#">Contact</a></li>        
+        </ul>
+    </li><li><a href="#">Mon panier</a>
+        <ul>
+            <li><a href="http://localhost/APP_INFO/panier.php">En cours</a></li>
+            <li><a href="#">Avant</a></li>  
+        </ul>
+    </li>
+    </li><li><a href="http://localhost/APP_INFO/recherche.php#">Rechercher</a> 
+    </li>
+    </li><li><a href="#">Nous rejoindre !</a>
+        <ul>
+            <li><a href="http://localhost/APP_INFO/formulaire_inscription.php#">S'inscire</a></li>
+            <li><a href="http://localhost/APP_INFO/formulaire_connexion.php#">Se connecter</a></li>           
+        </ul>
+    </li>
+</ul>
 
                 <!--Titre de la page-->
                 <h2> Bienvenue sur "Jamais sans ma pomme"<br> 
@@ -37,16 +65,16 @@
 
             <!--On définit tous les champs nécessaires à la connexion-->
             <form action="formulaire_connexion.php" method="post">
-                Pseudo : <input type="text" name="identifiant_u"/></br>
-                Mot de passe : <input type="password" name="mot_passe_u" /></br>
+               <p style = "font-family:across the stars ; color:#E8CC06" >Pseudo : <input type="text" name="identifiant_u"/></br>
+                Mot de passe : <input type="password" name="mot_passe_u" /></br></p>
                 <!--On définit un bouton permettant d'envoyer les informations-->
                 <input type="submit" name="envoyer" Value="Se connecter"/></br>
             </form>
     
             <div id="footer"> <!--bandeau en bas , à répéter sur toutes les pages web -->
-                <p><a href="Contact.html">- Contact -</a> </p>
-                <p><a href="mentions_legales.html">Mentions Legales - </p>
-                <p>Aide - </p>
+            
+                <p><a href="mentionslegales.html">- Mentions Legales - </p> 
+                
             </div>
 
         </body>
