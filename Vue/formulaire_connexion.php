fa55904ad99d913612e183eed358082fcc4de5c4
Chloé <chloe.van.cauwenberghe@gmail.com>
2015-05-05 15:22:16 +0200

    Correction

--- Vue/formulaire_connexion.php
+++ Vue/formulaire_connexion.php
@@ -14,8 +14,7 @@
 
         <!--On définit tous les champs nécessaires à la connexion-->
         <form action="section_membre.php" method="post">
-            <p style = "font-family:across the stars ; color:#E8CC06" >
-                Pseudo : <input type="text" name="identifiant_u"/></br>
+            <p> Pseudo : <input type="text" name="identifiant_u"/></br>
                 Mot de passe : <input type="password" name="mot_passe_u" /></br>
             </p>
             <!--On définit un bouton permettant d'envoyer les informations-->
--- Vue/section_membre.php
+++ Vue/section_membre.php
@@ -4,9 +4,8 @@
 
         <?php 
             include('simpleuh.php'); 
-        ?>
 
-        <?php
+
             // On démarre la session (ceci est indispensable dans toutes les pages de notre section membre)
             session_start ();
             // On récupère nos variables de session
