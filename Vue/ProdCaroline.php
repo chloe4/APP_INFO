f45bfc40f74305adc8d6ca6d5cdc326d196a2cd1
alice_samaran@orange.fr <alice_samaran@orange.fr>
2015-05-06 14:52:03 +0200

    blblbl

--- Vue/ProdCaroline.php
+++ Vue/ProdCaroline.php
@@ -7,24 +7,20 @@ include ('simpleuh.php')
 
 	<h3> Producteur : Caroline Nopper </h3>
 <center><img id="producteur" src ="http://fr.web.img5.acsta.net/medias/nmedia/18/63/18/02/18757972.jpg"/></center>
-<p style="color:black;font-family:Georgia"> Bonjour, <br> productrice depuis mon plus jeune âge, je chouchoute tous les fruits et légumes que je cultive : <br> </p>
+<p style="color:black;font-family: 'Satisfy', cursive;"> Bonjour, <br> productrice depuis mon plus jeune âge, je chouchoute tous les fruits et légumes que je cultive : <br> </p>
 
 <ul id="description">
-    <li style="color:black;font-family:Georgia"> des carottes</li>
-	<li style="color:black;font-family:Georgia"> des comcombres</li>
-	<li style="color:black;font-family:Georgia"> des courgettes</li>
+    <li style="color:black;font-family: 'Satisfy', cursive;"> des carottes</li>
+	<li style="color:black;font-family: 'Satisfy', cursive;"> des comcombres</li>
+	<li style="color:black;font-family: 'Satisfy', cursive;"> des courgettes</li>
 </ul>
 
- <a href ="file:///Users/caroline/Desktop/fairemescourses.html"/>Faire mes courses</a>
+ <a href ="file:///Users/caroline/Desktop/fairemescourses.html"/><p style="font-family: 'Satisfy', cursive;"/>Faire mes courses
 
-	<div id="footer">
-				 <p style="color:black;font-family:Georgia"><a href="mentionslegales.html">- Mentions Legales - </p>	
-				
-			
-				
-			
-		</div>
+	<?php 
+				include('bandeau_bas.php'); 
+	 		?>
 
 
-</body>
+
 </html>
--- Vue/Qui_sommes_nous.php
+++ Vue/Qui_sommes_nous.php
@@ -10,6 +10,7 @@
 	<h5><center> Qui sommes nous ?</center></h5>
 	<div id="fond_p">	
 		<p> Depuis janvier 2015, "Jamais sans ma pomme" accompagne les consommateurs de bons produits dans un choix de fruits et légumes bio exclusivement developpé par notre entreprise.</br>
+			
 			<strong>Acteurs majeurs</strong> de l'activité, Alice, Chloé, Caroline, Xavier et Côme veillent au bon déroulement de l'activité internet, aux échanges commerciaux et à la vie communautaire du site grâce aux forums et aux comptes clients.
 		</p>
 	</div>	
