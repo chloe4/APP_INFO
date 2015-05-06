457fbfaf2c592c469f03ceaa4d93c6b2f92fd1fa
Chloé <chloe.van.cauwenberghe@gmail.com>
2015-05-06 09:22:25 +0200

    Suppression fichiers

--- Vue/section_membre.php
+++ Vue/section_membre.php
@@ -4,8 +4,9 @@
 
         <?php 
             include('simpleuh.php'); 
+        ?>
 
-
+        <?php
             // On démarre la session (ceci est indispensable dans toutes les pages de notre section membre)
             session_start ();
             // On récupère nos variables de session
