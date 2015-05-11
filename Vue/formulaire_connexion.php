6e7cf321628f5ef2093a7e7c757567b2f10ead8f
Chloé <chloe.van.cauwenberghe@gmail.com>
2015-05-11 13:19:49 +0200

    formulaire connexion

--- Vue/formulaire_connexion.php
+++ Vue/formulaire_connexion.php
@@ -8,6 +8,13 @@
         vers la base de données-->
         <?php include('C:\wamp\www\APP_INFO\Modele\connexion_php.php'); ?>
 
+            <!--On connecte la page PHP avec la base de données via le fichier config.php / Toutes les informations écrites sur la page sont envoyées 
+            vers la base de données-->
+        <?php
+            include('C:\wamp\www\APP_INFO\Modele\connexion_php.php'); 
+        ?>
+<p> Si vous n'ètes pas inscris , créez votre compte<a href="http://localhost/APP_INFO/Vue/formulaire_inscription.php#"/>
+<input type="submit" name="envoyer" Value="s'inscrire"/></br></p>
         <!--On définit tous les champs nécessaires à la connexion-->
         <form action="section_membre.php" method="post">
             <p> Pseudo : <input type="text" name="identifiant_u"/></br>
@@ -15,8 +22,10 @@
             </p>
             <!--On définit un bouton permettant d'envoyer les informations-->
             <input type="submit" name="envoyer" Value="Se connecter"/></br>
+        
         </form>
-    
+        
+   
         <?php 
             include('bandeau_bas.php'); 
         ?>
