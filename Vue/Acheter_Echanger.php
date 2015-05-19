2a8c0046a0656a85a1ffc55f1c7f2a77d4356a81
alice_samaran@orange.fr <alice_samaran@orange.fr>
2015-05-19 14:32:18 +0200

    Page d'achat à relier à selectionner sur page produit

---
+++ Vue/Acheter_Echanger.php
@@ -0,0 +1,48 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+
+    <html>
+
+        <?php include('simpleuh.php');  ?>
+
+        <!--On connecte la page PHP avec la base de données via le fichier config.php / Toutes les informations écrites sur la page sont envoyées 
+        vers la base de données-->
+        <?php include('C:\wamp\www\APP_INFO\Modele\inscription_php.php'); ?>
+
+        <!--On définit tous les champs nécessaires à lsa page membre-->
+<center><form method="post" action="section_membre.php">
+
+
+  <!--QUantité du produit -->
+       <p>Selectionnez la quantité :<br />
+       <input type="number" value="1" max="50" min="0" step="1"><br />
+   </p>
+
+  <!--Selectionner pays -->
+
+  <p> Option 1 : </p>
+   <p> Dans quel département  ?<br/>
+              <select name="localisation_u" id="departement">
+                <optgroup label="Ile-de-france">
+                  <option value="paris">Paris</option>
+                  <option value="val-de-marne">Val-de-Marne</option>    
+                </optgroup>
+                <optgroup label="Midi-Pyrénées">
+                 <option value="haute-garonne">Haute-Garonne</option>
+                 <option value="gers">Gers</option>
+                </optgroup>
+              </select>
+          </p>
+           
+    <p> Option 2 :</p>
+    <p>  <a href="carte.html">Géolocalisez-vous</a></p>   
+
+            <input type="submit" value="Ajouter au panier"/>
+        
+            
+        </form></center>
+    
+        <?php include('bandeau_bas.php'); ?>
+
+    </html>
--- Vue/Liste_produits.php
+++ Vue/Liste_produits.php
@@ -7,11 +7,11 @@
 		?>
 
 		<?php
- 			$db = new PDO("mysql:host=127.0.0.1;dbname=login","root","");
+ 			$db = new PDO("mysql:host=127.0.0.1;dbname=jsmp","root","");
 	        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 	        // On selectionne les données dont on a besoin, c'est-à-dire le mot de passe et le pseudo de l'utilisateur
-	        $sql = "SELECT * FROM articles";
+	        $sql = "SELECT * FROM catalogue_articles";
 	        // Pas la peine de préparer la requête, pas de données récupéré depuis l'utilisateur
 	        // Du coup "query" directement
 	        $request = $db->query($sql);
