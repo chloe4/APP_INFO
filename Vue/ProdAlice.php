27e9ae03abbb4e30393cc5d7c730787cbb012001
caroline <caroline.nopper@laposte.net>
2015-05-19 15:08:56 +0200

    page producteur Alice
    création de la page

---
+++ Vue/ProdAlice.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<?php 
+include ('simpleuh.php')
+ ?>
+
+	<h3> Producteur : Alice Samaran </h3>
+<center><img id="producteur" src ="http://www.google.fr/url?source=imglanding&ct=img&q=http://www.media-web.fr/upload-saintnazaire-infos/cms/paragraphes/img/l/les-legumes-vont-directement-du-producteur-au-consommateur--1442.jpg&sa=X&ei=GjVbVf_XG4L_UpmGgIgL&ved=0CAkQ8wc&usg=AFQjCNGu8u12pv5yiMWLq6-CxL2oz6JSIg"/></center>
+<p style="color:black;font-family: 'Satisfy', cursive;"> Bonjour, <br> productrice depuis deux ans, je m'occupe de mes fruits et légumes chaque jour avec la plus grande attention, je cultive : <br> </p>
+
+<ul id="description">
+    <li style="color:black;font-family: 'Satisfy', cursive;"> des pommes</li>
+	<li style="color:black;font-family: 'Satisfy', cursive;"> des poires</li>
+	<li style="color:black;font-family: 'Satisfy', cursive;"> des pommes de terre</li>
+</ul>
+
+ <a href ="file:///Users/caroline/Desktop/fairemescourses.html"/><p style="font-family: 'Satisfy', cursive;"/>Faire mes courses
+
+	<?php 
+				include('bandeau_bas.php'); 
+	 		?>
+
+
+
+</html>
