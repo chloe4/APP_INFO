9f39ca7f73ff15444c28e997ba50606bf93772a7
caroline <caroline.nopper@laposte.net>
2015-05-19 15:18:27 +0200

    pages producteurs
    lien avec les pages producteurs et la page « faire courses »

--- Vue/faire_courses.php
+++ Vue/faire_courses.php
@@ -12,10 +12,10 @@
 			<h4> Mes producteurs favoris </h4>
 			<ul> 
 				<li> <a href ="ProdCaroline.php"> Caroline Nopper </a></li>
-				<li> <a href ="http://localhost/APP_INFO/Vue/ProdAlice.php"> Alice Samaran </a></li>
-				<li> <a href ="http://localhost/APP_INFO/Vue/ProdCome.php"> Côme Schechter </a></li>
-				<li> <a href ="http://localhost/APP_INFO/Vue/ProdXavier.php"> Xavier Gouesnard </a></li>
-				<li> <a href ="http://localhost/APP_INFO/Vue/ProdChloé.php"> Chloé Van Cauwenberghe </a></li>
+				<li> <a href ="ProdAlice.php"> Alice Samaran </a></li>
+				<li> <a href ="ProdCome.php"> Côme Schechter </a></li>
+				<li> <a href ="ProdXavier.php"> Xavier Gouesnard </a></li>
+				<li> <a href ="ProdChloé.php"> Chloé Van Cauwenberghe </a></li>
 			</ul>
 		</div>
 		
