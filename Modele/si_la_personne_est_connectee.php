6bedf91edee42e9318717c265d81ff38a131a2d5
alice_samaran@orange.fr <alice_samaran@orange.fr>
2015-05-23 13:47:11 +0200

    modif sur si la pers est connectée pour avoir menu deroulant

--- Modele/si_la_personne_est_connectee.php
+++ Modele/si_la_personne_est_connectee.php
@@ -1,8 +1,13 @@
 <?php 
 		if(isset($_SESSION["identifiant_u"])){
-			echo "<li><h3>Bonjour ".$_SESSION['identifiant_u']."</h3></li>";
+			echo "<li><p><a href='#'/>Bonjour ".$_SESSION['identifiant_u']."</p>";
+			echo"<ul>";
+			echo '<li><p><a href="membre_description.php">Modifier mon profil</a></p></li>';
+			echo '<li><p><a href="deconnexion.php">Déconnexion</a></p></li>';
+			echo"</ul>";
+			echo"</li>";
             echo '</br>';
             // On affiche un lien pour fermer notre session
-            echo '<a href="deconnexion.php">Déconnexion</a>';
+           
 		}
 	?>
