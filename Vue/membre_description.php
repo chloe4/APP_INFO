78315016e94426f4ed2e216f39515723646b6777
alice_samaran@orange.fr <alice_samaran@orange.fr>
2015-05-26 15:29:03 +0200

    huhuhuhu

--- Vue/membre_description.php
+++ Vue/membre_description.php
@@ -9,27 +9,28 @@
         <!--On connecte la page PHP avec la base de données via le fichier config.php / Toutes les informations écrites sur la page sont envoyées 
         vers la base de données-->
         <?php include('C:\wamp\www\APP_INFO\Modele\inscription_php.php'); ?>
+        <?php include('C:\wamp\www\APP_INFO\Modele\ajout_php.php'); ?>
 
         <!--On définit tous les champs nécessaires à lsa page membre-->
-		<form method="post" action="section_membre.php">
+<h1> Page d'offre de Fruits & Légumes</h1>
 
-	  		<!--Produits à vendre-->
-	       	<p>Cochez les aliments que vous voulez vendre en fruit :<br/>
-	       		<input type="checkbox" name="nom_a"  /><label for="banane"> Banane</label><input type="number" value="1" max="50" min="0" step="1"><br />
-			</p>
-	   		<p>Cochez les aliments que vous voulez vendre en légume :<br />
-				<input type="checkbox" name="nom_a" /> <label for="pomme de terre">Pomme de terre</label><input type="number" value="1" max="50" min="0" step="1"><br />
-	   		</p>
-	   		
-	   		<!--Petite description sur la personne-->
-			<form method="post" action="section_membre.php">
-		   		<p>
-		       		<p>Parlez nous de vous</p><br/>
-		       		<textarea name="ameliorer" id="ameliorer" rows="10" cols="50">Etes vous une patate ?!</textarea>       
-		   		</p>
-			</form>
+  <form action="" method="post">
            
-        	<input type="submit" value="Enregistrer"/>
+            <p> Type de produit :<select name="type_a" id="type">
+                                    <option value="fruit">Fruit</option>
+                                    <option value="legume">Légumes</option>
+                                </select></br>
+                Nom du produit : <input type="checkbox" name="nom_a" />Banane</br>
+                Quantité : <input type="number" value="1" max="50" min="0" step="1" name="id_o"><br />
+                Variété du produit : <input type="int" name="varietes_a" /></br>
+                Prix : <input type="number" value="1" max="50" min="0" step="0,25" name="montant_total_o"><br />
+                Commentaire sur le produit : <input type="text" name="description_a" /></br>
+            </p>
+                 
+            <!--On définit un bouton permettant d'envoyer les informations-->
+            <input type="submit" name="envoyer" Value="Ajout d'article"/>
+
+
         	<input type="submit" value="Modifier" /></br>
             
         </form>
