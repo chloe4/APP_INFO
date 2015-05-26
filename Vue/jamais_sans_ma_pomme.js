b03cbe17d24531771268131fd145e72a0e9438e9
alice_samaran@orange.fr <alice_samaran@orange.fr>
2015-05-26 14:42:09 +0200

    News

---
+++ Vue/jamais_sans_ma_pomme.js
@@ -0,0 +1,14 @@
+var rouge = [0, 100, 63];
+var orange = [40, 100, 60];
+var vert = [75, 10, 40];
+var bleu = [196, 77, 55];
+var violet = [28, 50, 60];
+
+var myName = "Bienvenue sur ";
+
+var letterColors = [red, orange, green, blue, purple];
+
+     bubbleShape = "circle";  
+
+drawName(myName,  letterColors);
+bounceBubbles();
