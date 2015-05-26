06333599b6dcee26955aa51e61bf5c6099e21b25
caroline <caroline.nopper@laposte.net>
2015-05-26 15:04:19 +0200

    changement design

--- Vue/bandeau_bas.php
+++ Vue/bandeau_bas.php
@@ -10,9 +10,6 @@
 
         </footer>
 
-		<div id="footer"> <!--bandeau en bas , à répéter sur toutes les pages web -->
-            <p><a href="mentions_legales.php">- Mentions Legales - </p>        
-        </div>
        </div>
 
     </body>
