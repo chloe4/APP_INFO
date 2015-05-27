d35ceb528b8e3d6164bdc9cb1cb4a0456375fe57
alice_samaran@orange.fr <alice_samaran@orange.fr>
2015-05-27 09:57:48 +0200

    huhu

--- Modele/si_la_personne_est_connectee.php
+++ Modele/si_la_personne_est_connectee.php
@@ -3,8 +3,10 @@
 			echo "<li>Bonjour ".$_SESSION['identifiant_u']." ";
             echo '</br>';
             echo '<ul>';
-            // On affiche un lien pour fermer notre session
-            echo '<li><a href="deconnexion.php">Déconnexion</a></li>';
+            // On affiche un lien pour fermer notre session            
             echo '<li><a href="page_faire_offre.php">Offre</a></li>';
+            echo '<li><a href="section_membre.php">Ma page</a></li>';
+            echo '<li><a href="membre_description.php">Modifier profil</a></li>';
+            echo '<li><a href="deconnexion.php">Déconnexion</a></li>';
             echo '</ul></li>';}
             ?>
