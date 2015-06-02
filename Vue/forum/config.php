db5f62a33a18d1cfb89d90afac11da4a1c47fbcf
xouf2114 <xavier.gouesnard@gmail.com>
2015-06-02 15:42:27 +0200

    forum recommité

---
+++ Vue/forum/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_type = 'mysqli';
+$db_host = 'localhost';
+$db_name = 'forum';
+$db_username = 'root';
+$db_password = '';
+$db_prefix = '';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_0a0553';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = '0edeab8dcc8f3c02';
+
+define('PUN', 1);
