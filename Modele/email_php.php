b12fcb700b77fe0c337fbac537dbfbefd0978f01
Chloé <chloe.van.cauwenberghe@gmail.com>
2015-06-04 20:41:47 +0200

    email

--- Modele/email_php.php
+++ Modele/email_php.php
@@ -11,22 +11,20 @@
             isset($_POST['prenom_u'])
         ){
 
-          $to=$_POST['Email'];
-          $subject=$_POST['Objet'];
-          $Prenom=$_POST['Prenom'];
-          $Nom=$_POST['Nom'];
-          $mail = new PHPMailer();
+          $to=$_POST['email_u'];
+          $subject=$_POST['objet'];
+          $Prenom=$_POST['prenom_u'];
+          $Nom=$_POST['nom_u'];
           $message=$_POST['commentaire'];
+          $mail = new PHPMailer();
           $msg='<html>
                   <body>
-                    <p><b>Vous nous avez posé une question </b>'.$Prenom.'<b> et nous vous en remercions</b></p></br>   
-                    <p>Le sujet : '.$subject.'</p>
-                    <p>Votre message est :"'.$message.'"</p></br></br>
-                    <p>Vous êtes : '.$Prenom.' '.$Nom.'</p>
+                    <p>Bonjour '.$Prenom.', nous avons bien reçu votre message.</p></br>   
+                    <p>De la part de : '.$Prenom.' '.$Nom.'</p>
                     <p>Votre email : '.$to.'</p></br></br>
-                    <p>Nous vous repondrons très prochainement.</p></br>
-                    <p>La team de BIO&BON vous remercie</p>
-                    <p>Le pole Com : bioetbon.isep@gmail.com</p>          
+                    <p>Objet : '.$subject.'</p>
+                    <p>Votre message :"'.$message.'"</p></br></br>
+                    <p>Nous vous répondrons le plus vite possible.</p></br>        
                   </body>
                 </html>';
           $mail->IsSMTP(); // enable SMTP
@@ -36,13 +34,14 @@
           $mail->Host = "smtp.gmail.com";
           $mail->Port = 465; // or 587
           $mail->IsHTML(true);
-          $mail->Username = "bioetbon.isep@gmail.com";
-          $mail->Password = "bioetbon";
+          $mail->Username = "jsmp.isep@gmail.com";
+          $mail->Password = "jsmpisep";
           $mail->CharSet = "utf-8";
-          $mail->SetFrom('bioetbon.isep@gmail.com');
+          $mail->SetFrom('jsmp.isep@gmail.com');
           $mail->Subject = $subject; ?> </br> <?php
           $mail->Body = $msg;
-          $mail->AddAddress($to,'quentin.lalle@gmail.com');
+          $mail->AddAddress($to);
+          $mail->AddAddress('chloe.van.cauwenberghe@gmail.com');
           /**/
 
 
@@ -50,7 +49,7 @@
             if ( !$mail->Send() ) {
               echo "Echec de l'envoi du mail, Erreur: " . $mail->ErrorInfo;
             } else {
-               ?> <div align="center"><h3 class="ok"><strong><?php echo "Message envoyé!"; ?></strong> </h3> </div> <?php
+               ?> <div align="center"><h3 class="ok"><strong><?php echo "Votre message a bien été envoyé."; ?></strong> </h3> </div> <?php
             }
         }
 
--- Vue/email.php
+++ Vue/email.php
@@ -15,7 +15,7 @@
                     Votre prénom : <input type="text" name="prenom_u"/></br>
                     Votre adresse mail : <input type="text" name="email_u"/></br>
                     Objet : <input type="text" name="objet"/></br>
-                    Contenu du message : <input type="text" name="commentaire"/></br>
+                    Contenu du message : <textarea type="text" name="commentaire" rows="10" cols="30"/></textarea></br>
                 </p>
                 <!--On définit un bouton permettant d'envoyer les informations-->
                 <input type="submit" name="envoyer" Value="Envoyer"/></br>
