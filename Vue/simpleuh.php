0cbd06a612c3930ab7c9594ad691e4249d4d081d
Chloé <chloe.van.cauwenberghe@gmail.com>
2015-06-05 16:35:40 +0200

    commit

--- Vue/simpleuh.php
+++ Vue/simpleuh.php
@@ -32,10 +32,5 @@
 					<?php include('C:\wamp\www\APP_INFO\Modele\si_la_personne_est_connectee.php'); ?>
 				</ul>
 			</nav>	
-			<div id="translator_3293">
-				<noscript>Javascript is disable 
-					- <a href="http://www.supportduweb.com/">http://www.supportduweb.com/</a> 
-					- <a href="http://www.supportduweb.com/generateur-boutons-traduction-translation-google-gratuit-html-code=script-boutons-traduire-page-web.html">Générateur de boutons de traduction</a>
-				</noscript>
-			</div><script type="text/javascript" src="http://services.supportduweb.com/translator/3293-2-ynnnnnnnn.js"></script>
+			
 		</header>
